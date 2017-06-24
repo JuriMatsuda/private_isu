1fa7745b69787e1673f54af49a9fd4ce76dad07d
Juri Matsuda <matsuda.juri@gmail.com>
2017-06-24 14:45:26 +0900

    lineprof使用

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -2,13 +2,13 @@ require 'sinatra/base'
 require 'mysql2'
 require 'rack-flash'
 require 'shellwords'
-# require 'rack-lineprof'
+require 'rack-lineprof'
 
 module Isuconp
   class App < Sinatra::Base
     use Rack::Session::Memcache, autofix_keys: true, secret: ENV['ISUCONP_SESSION_SECRET'] || 'sendagaya'
     use Rack::Flash
-    # use Rack::Lineprof, profile: 'app.rb'
+    use Rack::Lineprof, profile: 'app.rb'
 
     set :public_folder, File.expand_path('../../public', __FILE__)
 
